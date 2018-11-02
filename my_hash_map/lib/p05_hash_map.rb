360a048ed1b9680af4c3ba36f2947606d5fb94e8
sameehkhan <sameehkhan1@gmail.com>
2018-11-02 16:50:28 -0700

    Implement #resize on HashMap

--- my_hash_map/lib/p05_hash_map.rb
+++ my_hash_map/lib/p05_hash_map.rb
@@ -14,8 +14,13 @@ class HashMap
   end
 
   def set(key, val)
-    @store[bucket(key)].append(key,val)
-    @count += 1
+    if include?(key)
+      @store[bucket(key)].update(key,val)
+    else 
+      @store[bucket(key)].append(key,val)
+      @count += 1
+    end 
+    resize! if @count > num_buckets 
   end
 
   def get(key)
@@ -39,12 +44,12 @@ class HashMap
     "#{@count}"
   end 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -56,6 +61,13 @@ class HashMap
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2){LinkedList.new}
+    @store.each do |list|
+      list.each do |node|
+        new_store[node.key.hash % new_store.length].append(node.key,node.val) 
+      end
+    end
+    @store = new_store
   end
   
   # def inspect
