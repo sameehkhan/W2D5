69affd2d3fe83258a53814e7586e31e6a0aa071a
sameehkhan <sameehkhan1@gmail.com>
2018-11-02 14:28:47 -0700

    Create hash set for all data types

--- my_hash_map/lib/p03_hash_set.rb
+++ my_hash_map/lib/p03_hash_set.rb
@@ -5,20 +5,30 @@ class HashSet
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
-
   def insert(key)
-  end
-
-  def include?(key)
+    unless self.include?(key)
+      self[key] << key
+      @count += 1
+      resize! if @count > num_buckets
+    end
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
+  end
+
+  def include?(key)
+    self[key].include?(key)
   end
 
   private
 
-  def [](num)
+  def [](key)
     # optional but useful; return the bucket corresponding to `num`
+    @store[key.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +36,13 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2){Array.new}
+    @store.each do |el|
+      el.each do |num|
+        new_store[num % new_store.length] << num
+      end
+    end
+    @store = new_store
   end
+
 end
